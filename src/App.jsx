81a6d62bde8aa197bc4c1616bf0620ccd8c3d55d
Awais <awaisali.skylinxtech@gmail.com>
2025-01-07 00:35:48 +0200

    Nav Bar UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Result from "./pages/Result";
+import BuyCredit from "./pages/BuyCredit";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import HeaderStyles from "../styles/Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={HeaderStyles.header_parent_div}>
+      <div>
+        <h1 className={HeaderStyles.h1}>
+          Remove the <br />
+          <span> background from </span> <br />
+          images for free.
+        </h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          <br /> Lorem Ipsum has been the industry&apos;s standard dummy text ever.
+        </p>
+        <div>
+          <input type="file" id="upload1" name="" hidden />
+          <label htmlFor="upload1" className={HeaderStyles.upload_button}>
+            <img
+              src={assets.upload_btn_icon}
+              alt="upload-icon"
+              style={{ width: "15px", height: "22px", marginRight: "10px" }}
+            />
+            <p> Upload your image</p>
+          </label>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
