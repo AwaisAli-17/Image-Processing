0c30c3fadd6f6a6ce21add642e2c6597163fe316
Awais <awaisali.skylinxtech@gmail.com>
2025-01-08 03:10:47 +0200

    Slider UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
+import Steps from "../components/Steps";
+import Slider from "../components/Slider";
+import Testimonials from "../components/Testimonials";
 
 function Home() {
   return (
     <>
       <Header />
+      <Steps />
+      <Slider />
+      {/* <Testimonials /> */}
     </>
   );
 }
