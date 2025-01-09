f89f504346cdad491ba61534fb04ac7b6d0fbcef
AwaisAli-17 <awais.ali1312@gmail.com>
2025-01-09 23:27:58 +0200

    Testimonial Continue

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 import TestimonialStyle from "../styles/Testimonials.module.css";
-import { assets } from "../assets/assets";
+import { assets, testimonialsData } from "../assets/assets";
 
 const Testimonials = () => {
   return (
     <div>
       <h1 className={TestimonialStyle.h1}>Customer Testimonials</h1>
-      <div>
-        <div className={TestimonialStyle.testimonial_parent_div}>
-          <img src={assets.upload_icon} alt="upload-icon" />
-          <p className={TestimonialStyle.testimonial_item_heading}>Upload image</p>
-          <p className={TestimonialStyle.testimonial_item_para}>
-            This is a demo text, will replace it later. This is a demo..
-          </p>
-          <img src={assets.upload_icon} alt="upload-icon" />
-        </div>
-
-        <div className={TestimonialStyle.testimonial_parent_div}>
-          <img src={assets.upload_icon} alt="upload-icon" />
-          <p className={TestimonialStyle.testimonial_item_heading}>Upload image</p>
-          <p className={TestimonialStyle.testimonial_item_para}>
-            This is a demo text, will replace it later. This is a demo..
-          </p>
-          <img src={assets.upload_icon} alt="upload-icon" />
-        </div>
+      <div className={TestimonialStyle.testimonial_main_div}>
+        {testimonialsData.map((item, index) => (
+          <div key={index} className={TestimonialStyle.testimonial_parent_div}>
+            <p>"</p>
+            <p>{item.text}</p>
+            <div>
+              <img src={item.image} alt="" />
+              <div>
+                <p>{item.author}</p>
+                <p>{item.jobTitle}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
